6c283281ed6737f584308989d494d5622f5d3544
endInMind <lijiaping1994117@gmail.com>
2017-12-14 19:09:28 +0800

    Hibernate test

--- GD-GatherDove/src/main/java/pers/Brown/Dove/Domain/User.java
+++ GD-GatherDove/src/main/java/pers/Brown/Dove/Domain/User.java
@@ -1,20 +1,50 @@
 package pers.Brown.Dove.Domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
 public class User {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "user_name")
 	private String userName;
 
+	@Column(name = "password")
 	private String passWord;
 
 	private String ConfirmPassword;
 
+	@Column(name = "first_name")
 	private String firstName;
 
+	@Column(name = "last_name")
 	private String lastName;
 
+	@Column(name = "email")
 	private String email;
 
+	@Column(name = "phone")
 	private int phone;
+
+	public User() {
+	};
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int aId) {
+		this.id = aId;
+	}
 
 	public String getUserName() {
 		return this.userName;
--- GD-GatherDove/src/main/java/pers/Brown/Dove/Repository/impl/UserDaoImpl.java
+++ GD-GatherDove/src/main/java/pers/Brown/Dove/Repository/impl/UserDaoImpl.java
@@ -1,7 +1,11 @@
 package pers.Brown.Dove.Repository.impl;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -20,23 +24,32 @@ public class UserDaoImpl implements UserDao {
 
 	// ------- Constants (static final) ----------------------------------------
 	// ------- Static Variables (static) ---------------------------------------
+
 	// ------- Instance Variables (private) ------------------------------------
+	@Autowired
+	private SessionFactory sessionfactory;
+
 	// ------- Constructors ----------------------------------------------------
 	// ------- Instance Methods (public) ---------------------------------------
 	@Override
 	public void register(User user) {
-		String sql = "insert into users values(?,?,?,?,?,?)";
-
-		this.jdbcTemplate.update(sql,
-				new Object[] { user.getUserName(), user.getPassWord(), user.getFirstName(), user.getLastName(),
-						user.getEmail(), user.getPhone() });
+		Session session = this.sessionfactory.openSession();
+		session.persist(user);
+		return;
+		// String sql = "insert isnto users values(?,?,?,?,?,?)";
+		//
+		// this.jdbcTemplate.update(sql,
+		// new Object[] { user.getUserName(), user.getPassWord(), user.getFirstName(), user.getLastName(),
+		// user.getEmail(), user.getPhone() });
 
 	}
 
 	@Override
 	public User findUserByUserNameAndPassword(String userName, String password) {
 		// TODO Auto-generated method stub
-		String sql = "Select * from users where userName =" + userName + "and passWord =" + password;
+		String sql = "Select * from users where userName = ? and passWord =?";
+		List<User> list = this.jdbcTemplate.queryForList(sql, new Object[] { userName, password }, User.class);
+		return list.get(0);
 	}
 	// ------- Instance Methods (protected) ------------------------------------
 
